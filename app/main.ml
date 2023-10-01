32ad3a3ee5990a51d54cf17805083309c50b6fab
Sabine <sabineschmaltz@gmail.com>
2023-10-01 19:29:23 +0200

    rearrange routes for better readability

--- app/main.ml
+++ app/main.ml
@@ -12,21 +12,29 @@ let error_handler =
 
 let () =
   let config = Config.load_config "./config.json" in
+
+  let server_routes =
+    [
+      Dream.scope ""
+        [ Auth.check_server_api_key ~api_key:config.api_key ]
+        Api.Generated_server_endpoints.routes;
+    ]
+  in
+  let client_routes =
+    [
+      Dream.scope ""
+        [ Auth.check_client_jwt ~jwt_secret:config.client_jwt_secret ]
+        Api.Generated_client_endpoints.routes;
+      Dream.get "/push" (fun _ ->
+          Dream.stream
+            ~headers:[ ("Content-Type", "text/event-stream") ]
+            Api.Server_sent_events.forward_messages);
+    ]
+  in
   (*Lwt.async Server_sent_events.message_loop;*)
   Dream.run ~error_handler @@ Dream.logger
   @@ Dream.sql_pool "postgresql://postgres:test@127.0.0.1:5432/ocaml_api"
   (*@@ Dream.origin_referrer_check*)
   @@ Dream.router
-       [
-         Dream.get "/" (fun _ -> Dream.html Home.render);
-         Dream.scope ""
-           [ Auth.check_server_api_key ~api_key:config.api_key ]
-           Api.Generated_server_endpoints.routes;
-         Dream.scope ""
-           [ Auth.check_client_jwt ~jwt_secret:config.client_jwt_secret ]
-           Api.Generated_client_endpoints.routes;
-         Dream.get "/push" (fun _ ->
-             Dream.stream
-               ~headers:[ ("Content-Type", "text/event-stream") ]
-               Api.Server_sent_events.forward_messages);
-       ]
+       ([ Dream.get "/" (fun _ -> Dream.html Home.render) ]
+       @ server_routes @ client_routes)
